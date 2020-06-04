a3872d27d9da906ca6ea97d41bd46a3f7aa1e856
Jonathan Pe <jonathanqpe@gmail.com>
2020-06-04 14:38:16 -0700

    Revert "Updates"
    This reverts commit c9e0ac1ff0a2441ee7db605c782e742c28a45ac2.

---
+++ src/components/resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/resume.css';
+
+export default class Resume extends React.Component {
+    render() {
+        return (
+            <div className="resume">
+                Resumé
+            </div>
+        );
+    }
+}
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    navHeight: 100,
+    navSmallHeight: 70,
+}
