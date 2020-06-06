1ece77f12db99488da98f9eb3b8ba2f74f967861
Jonathan Pe <jonathanqpe@gmail.com>
2020-06-05 17:04:53 -0700

    Create theme and baseline for CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Navbar from './components/navbar.js';
 import Hero from './components/hero.js';
 import Resume from './components/resume.js';
@@ -7,15 +8,29 @@ import Music from './components/music.js';
 import League from './components/league.js';
 import colors from './config/colors';
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          backgroundColor: colors.darkBlue,
+          color: colors.white,
+          fontFamily: "'Poppins', sans-serif",
+        }
+      }
+    }
+  }
+});
+
 const useStyles = makeStyles((theme) => ({
   main: {
-      counterReset: "section 0",
-      minHeight: "100vh",
-      color: colors.white,
-      margin: "0 auto",
-      padding: "0px 150px",
-      maxWidth: 1600,
-      width: "100%",
+    counterReset: "section 0",
+    minHeight: "100vh",
+    color: colors.white,
+    margin: "0 auto",
+    padding: "0px 150px",
+    maxWidth: 1600,
+    width: "100%",
   },
 }));
 
@@ -23,14 +38,18 @@ export default function App() {
   const styles = useStyles();
 
   return (
-    <div>
-      <Navbar/>
-      <div className={styles.main}>
-        <Hero />
-        <Resume />
-        <Music />
-        <League />
-      </div>
-    </div>
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <div className={styles.main}>
+          <Hero />
+          <Resume />
+          <Music />
+          <League />
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
+    
   );
 }
